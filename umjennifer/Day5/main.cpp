c3ba8f912368a275d8f7e7e95bc36164ec85ee2e
Jenn <um.jennifer@gmail.com>
2020-12-14 22:29:15 -0800

    Update main.cpp

--- umjennifer/Day5/main.cpp
+++ umjennifer/Day5/main.cpp
@@ -1,6 +1,6 @@
 /*
 Advent of Code 2020 Day 5
-v1, Last Modified: 2020-12-13
+v1, Last Modified: 2020-12-14
 */
 
 #include <iostream>
@@ -15,81 +15,70 @@ struct Range {
     int end;
 };
 
-struct Passes {
+struct BoardingPass {
     int row;
     int column;
-    int seatID = row * column;
+    //int seatID = (row * 8) + column;
+    int seatID;
 };
 
 int main(){
     vector <string> input;
     fileLogistics(input);
-    /*
-     FBFBBFFRLR
-     Start by considering the whole range, rows 0 through 127.
-     
-     F means to take the lower half, keeping rows 0 through 63.
-     B means to take the upper half, keeping rows 32 through 63.
-     F means to take the lower half, keeping rows 32 through 47.
-     B means to take the upper half, keeping rows 40 through 47.
-     B keeps rows 44 through 47.
-     F keeps rows 44 through 45.
-     The final F keeps the lower of the two, row 44.
-     */
+    vector<BoardingPass> passes;
     
-
-    
-    string test = "FBFBBFF";
-    
-    Range crr;
-    
-    crr.start = 0;
-    crr.end = 127;
-    
-    for (int i = 0 ; i < test.size(); i++){
+    for (int i = 0; i < input.size(); i++){
+        BoardingPass curr_pass;
+        string this_pass = input.at(i);
         
-        if (test[i] == 'F'){
-            // end = 63
-            // start = 32
-            crr.end = (crr.end + crr.start) / 2;
-            cout << "F @ index " << i << " crr.start=" << crr.start << " crr.end=" << crr.end << endl;
+        // current row range
+        Range crr;
+        crr.start = 0;
+        crr.end = 127;
+        for (int j = 0; j < 7; j++){ // check row
+            if (this_pass[j] == 'F'){
+                crr.end = (crr.end + crr.start) / 2;
+                //cout << "F @ index " << j << " crr.start=" << crr.start << " crr.end=" << crr.end << endl;
+            }
+            else if (this_pass[j] == 'B'){
+                crr.start = ((crr.end + crr.start) / 2) + 1;
+                //cout << "B @ index " << j << " crr.start=" << crr.start << " crr.end=" << crr.end << endl;
+            }
         }
-        else if (test[i] == 'B'){
-            // start = 32
-            // end = 47
+        
+        // current column range
+        Range ccr;
+        ccr.start = 0;
+        ccr.end = 7;
+        for (int k = 7; k < this_pass.size(); k++){
             
-            // need: start = 40. end = 47
-            crr.start = ((crr.end + crr.start) / 2) + 1;
-            cout << "B @ index " << i << " crr.start=" << crr.start << " crr.end=" << crr.end << endl;
+            if (this_pass[k] == 'L'){
+                ccr.end = (ccr.end + ccr.start) / 2;
+                //cout << "L @ index " << k << " ccr.start=" << ccr.start << " ccr.end=" << ccr.end << endl;
+            }
+            else if (this_pass[k] == 'R'){
+                ccr.start = ((ccr.end + ccr.start) / 2) + 1;
+                //cout << "R @ index " << k << " ccr.start=" << ccr.start << " ccr.end=" << ccr.end << endl;
+            }
+        }
+        //cout << endl << endl;
+        curr_pass.row = crr.start;
+        curr_pass.column = ccr.start;
+        curr_pass.seatID = (curr_pass.row * 8) + curr_pass.column;
+        passes.push_back(curr_pass);
+    }
+    
+    int max_seatid = passes.at(0).seatID;
+    for (int i = 0; i < passes.size(); i++){
+        cout << "pass" << i << " row=" << passes.at(i).row << " col=" << passes.at(i).column << " seatid=" << passes.at(i).seatID << endl;
+        if (passes.at(i).seatID > max_seatid){
+            max_seatid = passes.at(i).seatID;
         }
     }
     
-    cout << endl;
-    /*
-    RLR:
-    Start by considering the whole range, columns 0 through 7.
-     
-    R means to take the upper half, keeping columns 4 through 7.
-    L means to take the lower half, keeping columns 4 through 5.
-    The final R keeps the upper of the two, column 5.
-     */
-    string test2 = "RLR";
-    Range ccr;
-    ccr.start = 0;
-    ccr.end = 7;
-    for (int i = 0 ; i < test2.size(); i++){
-        
-        if (test2[i] == 'L'){
-            // end = 63
-            // start = 32
-            ccr.end = (ccr.end + ccr.start) / 2;
-            cout << "L @ index " << i << " ccr.start=" << ccr.start << " ccr.end=" << ccr.end << endl;
-        }
-        else if (test2[i] == 'R'){
-            ccr.start = ((ccr.end + ccr.start) / 2) + 1;
-            cout << "R @ index " << i << " ccr.start=" << ccr.start << " ccr.end=" << ccr.end << endl;
-        }
-    }
+    cout << "max seat id = " << max_seatid << endl;
+    
+    
     
     return 0;
 }

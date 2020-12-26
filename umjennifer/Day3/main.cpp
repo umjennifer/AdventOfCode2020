be1b56989cb2b873391a02680545a114147daa26
Jenn <um.jennifer@gmail.com>
2020-12-25 19:13:42 -0800

    Completed part two!

--- umjennifer/Day3/main.cpp
+++ umjennifer/Day3/main.cpp
@@ -1,7 +1,7 @@
 /*
     Jennifer Um
     Advent of Code Day 3
-    v1.2, 2020-12-25
+    v1.3, 2020-12-25
  */
 
 #include <iostream>
@@ -18,6 +18,7 @@ struct Coordinate {
 
 void saveInputInVector(string, vector<string>&);
 void outputMatrix(char *arr, unsigned long rows, unsigned long cols);
+int partOne(vector<string>, int, int);
 
 
 int main(){
@@ -25,55 +26,22 @@ int main(){
     vector<string> inputVector;
     saveInputInVector(filename, inputVector);
     
+    int product = 1;
+//    cout << partOne(inputVector, 1, 1) << endl;
+//    cout << partOne(inputVector, 3, 1) << endl;
+//    cout <<  partOne(inputVector, 5, 1) << endl;
+//    cout <<  partOne(inputVector, 7, 1) << endl;
+//    cout <<  partOne(inputVector, 1, 2) << endl;
     
-    // create matrix_template
-    unsigned long matrix_rows = inputVector.size();
-    unsigned long matrix_cols = inputVector.at(0).size();
-    char matrix_template[matrix_rows][matrix_cols];
-    // populate matrix_template with vals from inputVector
-    for (int i = 0; i < inputVector.size(); i++){
-        for (int j = 0; j < inputVector.at(i).size(); j++){
-            matrix_template[i][j] = inputVector.at(i).at(j);
-        }
-    }
-    
-    //outputMatrix((char *)matrix_template, matrix_rows, matrix_cols);
-    
-    int col = 0;
-    int right_step = 3;
-    int down_step = 1;
-    int bounds_adjust = right_step - 1;
-    
-    int total_trees = 0;
-    
-    for (int i = 0; i < matrix_rows; i++){
-        int row = i+down_step;
-        int largest_index = matrix_cols - 1;
-        cout << "row:" << row << " ";
-        
-        if ((col + right_step) > largest_index){ // 8+3 > 10
-            col = (col - (largest_index)) + bounds_adjust;
-            cout << "col:" << col << " ";
-        }
-        else {
-            col += right_step;
-            cout << "col:" << col << " ";
-        }
-        
-        // determine symbol given row and col
-        char symbol = matrix_template[row][col];
-        cout << "symbol:" << symbol << endl;
-        if (symbol == '#'){
-            total_trees++;
-        }
-        
-    }
-    
-    cout << "total trees: " << total_trees << endl;
-    
+    // partOne(inputVector, right_step, down_step);
+    product *= partOne(inputVector, 1, 1);
+    product *= partOne(inputVector, 3, 1);
+    product *= partOne(inputVector, 5, 1);
+    product *= partOne(inputVector, 7, 1);
+    product *= partOne(inputVector, 1, 2);
 
+    cout << "What do you get if you multiply together the number of trees encountered on each of the listed slopes? \nAnswer:" << product << endl;
     
-
 }
 
 void saveInputInVector(string filename, vector<string>& inputVector){
@@ -98,4 +66,57 @@ void outputMatrix(char *arr, unsigned long rows, unsigned long cols){
         }
         cout << endl;
     }
+}
+
+int partOne(vector<string> inputVector, int right_step, int down_step){
+    // create matrix_template
+    unsigned long matrix_rows = inputVector.size();
+    unsigned long matrix_cols = inputVector.at(0).size();
+    char matrix_template[matrix_rows][matrix_cols];
+    // populate matrix_template with vals from inputVector
+    for (int i = 0; i < inputVector.size(); i++){
+        for (int j = 0; j < inputVector.at(i).size(); j++){
+            matrix_template[i][j] = inputVector.at(i).at(j);
+        }
+    }
+    
+    //outputMatrix((char *)matrix_template, matrix_rows, matrix_cols);
+    
+    int col = 0;
+    int row = 0;
+    int bounds_adjust = right_step - 1;
+    
+    int total_trees = 0;
+    
+    for (int i = 0; i < matrix_rows; i++){
+        
+        if (row + down_step > matrix_rows){
+            break;
+        }
+        else {
+            row += down_step;
+        }
+        
+        int largest_index = matrix_cols - 1;
+        //cout << "row:" << row << " ";
+        
+        if ((col + right_step) > largest_index){ // 8+3 > 10
+            col = (col - (largest_index)) + bounds_adjust;
+            //cout << "col:" << col << " ";
+        }
+        else {
+            col += right_step;
+            //cout << "col:" << col << " ";
+        }
+        
+        // determine symbol given row and col
+        char symbol = matrix_template[row][col];
+        //cout << "symbol:" << symbol << endl;
+        if (symbol == '#'){
+            total_trees++;
+        }
+        
+    }
+    //cout << "total trees: " << total_trees << endl;
+    return total_trees;
 }

9b5c9c015da900e2cd4c917648b63912d26eeeaa
Jenn <um.jennifer@gmail.com>
2020-12-25 18:50:55 -0800

    Completed part 1!

--- umjennifer/Day3/main.cpp
+++ umjennifer/Day3/main.cpp
@@ -1,109 +1,101 @@
-
-//
-//  main.cpp
-//  Day3
-//
-//  Created by Jennifer Um on 12/3/20.
-//  Copyright © 2020 Jennifer Um. All rights reserved.
-//
-
-struct Coordinate {
-    int x;
-    int y;
-};
+/*
+    Jennifer Um
+    Advent of Code Day 3
+    v1.2, 2020-12-25
+ */
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <vector>
+
 using namespace std;
 
+struct Coordinate {
+    int x = 0;
+    int y = 0;
+};
+
+void saveInputInVector(string, vector<string>&);
+void outputMatrix(char *arr, unsigned long rows, unsigned long cols);
+
+
 int main(){
-    // can be class members
-    int width;
-    int length;
+    string filename = "input.txt";
+    vector<string> inputVector;
+    saveInputInVector(filename, inputVector);
     
-    ifstream fin;
-    string line;
-    vector<string> linesOfForest;
-
-    fin.open("input.txt");
-
-    while(!fin.eof()) {
-        //cout << "reading line" << endl;
-        getline(fin,line);
-        //cout << line << endl;
-        linesOfForest.push_back(line);
-    }
-    fin.close();
     
-    width = linesOfForest[0].size();
-    length = linesOfForest.size();
-
-    /*
-    for (int i = 0; i < linesOfForest.size(); i++){
-        cout << linesOfForest.at(i) << endl;
-    }
-    */
-    //cout << linesOfForest[1][1] << endl;
-
-    
-    // Create 2D array from the vector
-    char forest[length][width];
-    cout << "length: " << length << endl;
-    cout << "width: " << width << endl;
-    
-    for (int i = 0; i < length; i++){
-        for (int j = 0; j < width; j++){
-            //forest[i][j] = '*';
-            //cout << linesOfForest[i][j];
-            forest[i][j] = linesOfForest[i][j];
-            //cout << linesOfForest[i][j];
+    // create matrix_template
+    unsigned long matrix_rows = inputVector.size();
+    unsigned long matrix_cols = inputVector.at(0).size();
+    char matrix_template[matrix_rows][matrix_cols];
+    // populate matrix_template with vals from inputVector
+    for (int i = 0; i < inputVector.size(); i++){
+        for (int j = 0; j < inputVector.at(i).size(); j++){
+            matrix_template[i][j] = inputVector.at(i).at(j);
         }
-        //cout << endl;
     }
     
-    /*
-    for (int i = 0; i < length; i++){
-        for (int j = 0; j < width; j++){
-            cout << forest[i][j];
-        }
-        cout << endl;
-    }*/
+    //outputMatrix((char *)matrix_template, matrix_rows, matrix_cols);
     
+    int col = 0;
     int right_step = 3;
     int down_step = 1;
+    int bounds_adjust = right_step - 1;
     
-    int curr_i = 0;
-    int curr_j = 0;
+    int total_trees = 0;
     
-    vector<Coordinate> coordinates;
-    // TODO: create constructor if I create class
-    Coordinate coord1;
-    coord1.x = 0;
-    coord1.y = 0;
-    coordinates.push_back(coord1);
-    
-    
-    // create all the coordinates
-    for (int i = 0; i < length; i++){
-            curr_j += down_step;
-            //curr_coord.y = curr_j;
-        cout << curr_j << endl;
+    for (int i = 0; i < matrix_rows; i++){
+        int row = i+down_step;
+        int largest_index = matrix_cols - 1;
+        cout << "row:" << row << " ";
+        
+        if ((col + right_step) > largest_index){ // 8+3 > 10
+            col = (col - (largest_index)) + bounds_adjust;
+            cout << "col:" << col << " ";
+        }
+        else {
+            col += right_step;
+            cout << "col:" << col << " ";
+        }
+        
+        // determine symbol given row and col
+        char symbol = matrix_template[row][col];
+        cout << "symbol:" << symbol << endl;
+        if (symbol == '#'){
+            total_trees++;
+        }
+        
     }
+    
+    cout << "total trees: " << total_trees << endl;
+    
 
-    for (int i = 0; i < width; i++){
-            curr_j += down_step;
-            //curr_coord.y = curr_j;
-        cout << curr_j << endl;
+    
+
+}
+
+void saveInputInVector(string filename, vector<string>& inputVector){
+    ifstream fin;
+    string line;
+
+    fin.open(filename);
+
+    // save all lines of input into vector "input"
+    while(!fin.eof()) {
+        getline(fin, line);
+        inputVector.push_back(line);
     }
-    
-    /*
-    // print out all the coordinates
-    for (int i = 0; i < coordinates.size(); i++){
-        cout << "coord x: " << coordinates[i].x << ", coord y: " << coordinates[i].y;
+    fin.close();
+}
+
+void outputMatrix(char *arr, unsigned long rows, unsigned long cols){
+    //output the matrix
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j< cols; j++){
+            cout<<*((arr+i*cols) + j);
+        }
         cout << endl;
-    }*/
-    
-    
+    }
 }
